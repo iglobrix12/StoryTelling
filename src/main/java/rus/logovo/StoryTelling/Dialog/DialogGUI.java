bcd5297b135f4c4b41ec56a8cc922d8af9358402
iglobrix12 <152422453+iglobrix12@users.noreply.github.com>
2025-04-02 21:44:38 +0300

    1.3-beta

--- src/main/java/rus/logovo/StoryTelling/Dialog/DialogGUI.java
+++ src/main/java/rus/logovo/StoryTelling/Dialog/DialogGUI.java
@@ -1,5 +1,6 @@
 package rus.logovo.StoryTelling.Dialog;
 
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
@@ -8,6 +9,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
+import net.minecraft.world.level.Level;
 
 public class DialogGUI extends Screen {
     private final String question;
@@ -16,20 +18,24 @@ public class DialogGUI extends Screen {
     private final EntityRenderDispatcher entityRenderer;
     private float entityRotationYaw = 0;
     private float entityRotationPitch = 0;
+    private final Level level;
 
     public DialogGUI(String question, Rest[] options, Entity entity) {
         super(Component.literal("Dialog"));
         this.question = question;
-        this.options = options;
+        this.options = options != null ? options : new Rest[0];
         this.entity = entity;
         this.entityRenderer = Minecraft.getInstance().getEntityRenderDispatcher();
+        this.level = Minecraft.getInstance().level;
     }
 
     @Override
     protected void init() {
         super.init();
         int buttonY = this.height / 2;
-        for (int i = 0; i < options.length; i++) {
+        int maxButtons = Math.min(options.length, 6); // Максимум 6 кнопок
+
+        for (int i = 0; i < maxButtons; i++) {
             int width = 200;
             int height = 20;
             int x = this.width / 2 - width / 2;
@@ -52,14 +58,15 @@ public class DialogGUI extends Screen {
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
         this.renderBackground(poseStack);
         drawCenteredString(poseStack, this.font, question, this.width / 2, this.height / 2 - 40, 0xFFFFFF);
-        renderEntity(poseStack, mouseX, mouseY);
+        renderEntity(poseStack, mouseX, mouseY, partialTick); // Добавлен partialTick
         super.render(poseStack, mouseX, mouseY, partialTick);
     }
 
-    private void renderEntity(PoseStack poseStack, int mouseX, int mouseY) {
+    private void renderEntity(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
         int entityX = this.width / 2;
         int entityY = this.height / 2 - 70;
         float scale = 50.0F; // Увел
+
 
         double deltaX = mouseX - entityX;
         double deltaY = mouseY - entityY;
@@ -79,15 +86,22 @@ public class DialogGUI extends Screen {
         poseStack.mulPose(Vector3f.YP.rotationDegrees(entityRotationYaw));
         poseStack.mulPose(Vector3f.XP.rotationDegrees(entityRotationPitch));
 
+        entityRenderer.setRenderShadow(false);
         entityRenderer.render(
                 entity,
                 0.0, 0.0, 0.0,
                 0.0F,
-                1.0F,
+                partialTick,
                 poseStack,
                 this.minecraft.renderBuffers().bufferSource(),
                 15728880
         );
+        this.minecraft.renderBuffers().bufferSource().endBatch();
         poseStack.popPose();
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 }
